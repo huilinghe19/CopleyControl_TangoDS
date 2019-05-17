141698e5fc5f20013ac8caf2c250d01fe10da777
huilinghe19 <huiling.he@helmholtz-berlin.de>
2019-05-17 11:48:32 +0200

    GUI works

--- testGUI.py
+++ testGUI.py
@@ -1,37 +1,86 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sys
+from taurus.qt.qtgui.display import *
 from taurus.external.qt import Qt
 from taurus.qt.qtgui.application import TaurusApplication
 from taurus.qt.qtgui.panel import TaurusForm
-from taurus.qt.qtgui.button import TaurusCommandButton
+from taurus.qt.qtgui.button import TaurusCommandButton, QButtonBox
 from taurus.qt.qtgui.plot import TaurusTrend
 
-
+Door_name = 'Door/demo2/1'
 app = TaurusApplication(sys.argv)
 panel = Qt.QWidget()
-vlayout = Qt.QVBoxLayout()
-panel.setLayout(vlayout)
+mainLayout = Qt.QVBoxLayout()
+panel.setLayout(mainLayout)
 
-hlayout = Qt.QHBoxLayout()
-vlayout.addLayout(hlayout)
+settingLayout = Qt.QHBoxLayout()
+mainLayout.addLayout(settingLayout)
+
+statelayout = Qt.QVBoxLayout()
+scanlayout = Qt.QVBoxLayout()
+settingLayout.addLayout(statelayout)
+settingLayout.addLayout(scanlayout)
 
 panel1 = TaurusForm()
 props = [ 'state', 'status', 'position', 'velocity', 'acceleration']
 model = [ 'stepnet01/%s' % p for p in props ]
 panel1.setModel(model)
-hlayout.addWidget(panel1)
+statelayout.addWidget(panel1)
 
-button =  TaurusCommandButton(command = "RunMacro", 
-                              parameters=['ascan','stepnet01','0','10000','5','1'],
-                              icon=None)
-button.setCustomText('Ascan')
-button.setModel('Door/demo2/1')
-hlayout.addWidget(button)
+scanmainLayout = Qt.QHBoxLayout()
+scanButtonLayout = Qt.QHBoxLayout()
+scanlayout.addLayout(scanmainLayout)
+scanlayout.addLayout(scanButtonLayout)
+
+lableLayout = Qt.QVBoxLayout()
+parametersLayout= Qt.QVBoxLayout()
+scanmainLayout.addLayout(lableLayout)
+scanmainLayout.addLayout(parametersLayout)
+
+l1 = TaurusLabel('Start position')
+l2= TaurusLabel('Final position')
+l3= TaurusLabel('Interval')
+l4= TaurusLabel('Integration time')
+
+lableLayout.addWidget(l1)
+lableLayout.addWidget(l2)
+lableLayout.addWidget(l3)
+lableLayout.addWidget(l4)
+
+w1 = Qt.QLineEdit()
+w2 = Qt.QLineEdit()
+w3 = Qt.QLineEdit()
+w4 = Qt.QLineEdit()
+parametersLayout.addWidget(w1)
+parametersLayout.addWidget(w2)
+parametersLayout.addWidget(w3)
+parametersLayout.addWidget(w4)
+def getResult(widget):
+    text = widget.displayText() 
+    return text.encode('utf-8')
+   
+scanbutton = TaurusCommandButton(command = "RunMacro", icon=None)
+scanbutton.setCustomText('Ascan')
+scanbutton.setModel(Door_name) 
+def setScan():
+    startPos = getResult(w1)
+    endPos = getResult(w2)
+    intervals = getResult(w3)
+    integrationTime = getResult(w4)
+    scanbutton.setParameters(['ascan', 'stepnet01', getResult(w1), getResult(w2), getResult(w3), getResult(w4)])
+
+setParameterbutton = Qt.QPushButton("Apply")
+setParameterbutton.clicked.connect(setScan)    
+scanButtonLayout.addWidget(setParameterbutton)
+scanButtonLayout.addWidget(scanbutton)
 
 panel2 = TaurusTrend()
 model2 = ['stepnet01/position']
 panel2.setXIsTime(True) 
 panel2.setModel(model2)
-vlayout.addWidget(panel2)
+mainLayout.addWidget(panel2)
 
 panel.show()
+panel.setWindowTitle("Copley Controller: Stepnet01")
 sys.exit(app.exec_())
