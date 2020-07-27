683e22dc51b489088992606312cdb8cecd958a7c
huilinghe19 <huiling.he@helmholtz-berlin.de>
2020-07-27 14:58:02 +0200

    GUI withcopleyCtrl tango DS

--- testGUI.py
+++ testGUI.py
@@ -7,12 +7,14 @@ from taurus.qt.qtgui.application import TaurusApplication
 from taurus.qt.qtgui.panel import TaurusForm
 from taurus.qt.qtgui.button import TaurusCommandButton, QButtonBox
 from taurus.qt.qtgui.plot import TaurusTrend
+from taurus.qt.qtgui.plot import TaurusPlot
 
-Door_name = 'Door/demo2/1'
+Door_name = 'Door/python3tangods/1'
 app = TaurusApplication(sys.argv)
 panel = Qt.QWidget()
 mainLayout = Qt.QVBoxLayout()
 panel.setLayout(mainLayout)
+
 
 settingLayout = Qt.QHBoxLayout()
 mainLayout.addLayout(settingLayout)
@@ -24,9 +26,11 @@ settingLayout.addLayout(scanlayout)
 
 panel1 = TaurusForm()
 props = [ 'state', 'status', 'position', 'velocity', 'acceleration']
-model = [ 'stepnet01/%s' % p for p in props ]
+model = [ 'tango://dide17.basisit.de:10000/motor/copleyctrl/1/%s' % p for p in props ]
 panel1.setModel(model)
 statelayout.addWidget(panel1)
+panel1.show()
+
 
 scanmainLayout = Qt.QHBoxLayout()
 scanButtonLayout = Qt.QHBoxLayout()
@@ -63,25 +67,84 @@ scanbutton.setModel(Door_name)
 
 def getResult(widget):
     text = widget.displayText() 
-    return text.encode('utf-8')
+    print("text: ", text)
+    print(type(text))
+    print(text.encode('utf-8'))
+    #return text.encode('utf-8')
+    return text
+def getStartPos():
+    print("Start:", getResult(w1))
+    print(type(getResult(w1)))
+    return str(getResult(w1))
+def getEndPos():
+    return str(getResult(w2))
+def getIntervals():
+    return int(getResult(w3))
+def getIntegrationTime():
+    return str(getResult(w4))
+scanbutton = TaurusCommandButton(command = "RunMacro", icon=None)
+scanbutton.setCustomText('Ascan')
+scanbutton.setModel(Door_name) 
 def setScan():
-    startPos = getResult(w1)
-    endPos = getResult(w2)
-    intervals = getResult(w3)
-    integrationTime = getResult(w4)
-    scanbutton.setParameters(['ascan', 'stepnet01', startPos, endPos, intervals, intervals])
+    scanbutton.setParameters(['ascan', 'tango://dide17.basisit.de:10000/motor/copleyctrl/1', getStartPos(), 
+                              getEndPos(), 
+                              getIntervals(), 
+                              getIntegrationTime()])
 
 setParameterbutton = Qt.QPushButton("Apply")
 setParameterbutton.clicked.connect(setScan)    
 scanButtonLayout.addWidget(setParameterbutton)
 scanButtonLayout.addWidget(scanbutton)
 
+
+
+
 panel2 = TaurusTrend()
-model2 = ['stepnet01/position']
-panel2.setXIsTime(True) 
+model2 = ['tango://dide17.basisit.de:10000/motor/copleyctrl/1/position', 'tango://dide17.basisit.de:10000/motor/copleyctrl/2/position']
+#panel2.setXIsTime(True) 
 panel2.setModel(model2)
 mainLayout.addWidget(panel2)
 
 panel.show()
-panel.setWindowTitle("Copley Controller: Stepnet01")
+panel.setWindowTitle("Copley Controller: Stepnet3")
+
+
+#panelKeithley = Qt.QWidget()
+#mainLayout2 = Qt.QVBoxLayout()
+#panelKeithley.setLayout(mainLayout2)
+
+#panel3 = TaurusForm()
+#props = [ 'state', 'value']
+#model = [ 'keithleySpannung/%s' % p for p in props ]
+#panel3.setModel(model)
+#mainLayout2.addWidget(panel3)
+
+
+#panel4 = TaurusTrend()
+#model4 = ['keithleySpannung/value']
+#panel4.setXIsTime(True) 
+#panel4.setModel(model4)
+#mainLayout2.addWidget(panel4)
+#panelKeithley.show()
+#panelKeithley.setWindowTitle("Keithley2000 Spannung")
+
+
+panelmotors = Qt.QWidget()
+mainLayout9 = Qt.QVBoxLayout()
+panelmotors.setLayout(mainLayout9)
+
+panel9 = TaurusForm()
+model9 = [ 'tango://dide17.basisit.de:10000/motor/copleyctrl/1','tango://dide17.basisit.de:10000/motor/copleyctrl/2','tango://dide17.basisit.de:10000/motor/copleyctrl/1/position', 'tango://dide17.basisit.de:10000/motor/copleyctrl/2/position']
+panel9.setModel(model9)
+mainLayout9.addWidget(panel9)
+
+scanbutton10 = TaurusCommandButton(command = "RunMacro", icon=None)
+scanbutton10.setCustomText('Start Scan')
+scanbutton10.setModel(Door_name) 
+scanbutton10.setParameters(['ascan', 'tango://dide17.basisit.de:10000/motor/copleyctrl/1', 1000, 10000, 5, 2])                      
+mainLayout9.addWidget(scanbutton10)
+
+panelmotors.show()
+panelmotors.setWindowTitle("Motors Status")
+
 sys.exit(app.exec_())
